78e85944344e92dafbf73edf692e12a3810f40a8
John Heger <heger7@gmail.com>
2023-12-08 14:11:32 -0600

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+  
+
+  function handleOnSubmit(newQuestion) {
+    setQuestions([...questions, newQuestion])
+  }
+
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((r) => r.json())
+    .then((questions) => setQuestions(questions));
+  }, []);
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onSubmit={handleOnSubmit} /> : <QuestionList questions={questions} setQuestions={setQuestions} />}
     </main>
   );
 }
