cf45248b3f7246025d8272f35a59f29616ce970d
John Heger <heger7@gmail.com>
2023-12-08 14:11:52 -0600

    completed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,32 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList( { questions, setQuestions }) {
+  console.log(questions);
+
+  function handleDeleteQuestion(deletedQuestion) {
+    const updatedQuestions = questions.filter((question) => question.id !== deletedQuestion.id);
+    setQuestions(updatedQuestions);
+  }
+
+  function handleUpdateDropdownAnswer(updatedAnswer) {
+    const updatedDropdownAnswer = questions.map((question) => {
+      if(question.id === updatedAnswer.id) {
+        return updatedAnswer
+      } else {
+        return question
+      }
+    })
+    setQuestions(updatedDropdownAnswer)
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map((question) => {
+        return <QuestionItem key={question.id} question={question} onDeleteQuestion={handleDeleteQuestion} onHandleUpdatedDropdownAnswer={handleUpdateDropdownAnswer}/>
+})}
+      </ul>
     </section>
   );
 }
